96ec2027d4363e80abbb108f10473de0feac7bcd
David Gullmarsvik <awt2.2012@gmail.com>
2012-10-30 12:44:05 +0100

    Uppgift 2

--- Lektion32/Models/Entities/User.cs
+++ Lektion32/Models/Entities/User.cs
@@ -19,23 +19,5 @@ namespace Lektion32.Models.Entities
         {
             // Not implemented: This method should send an email to the user with subject and message
         }
-
-        public void Save()
-        {
-            var repo = new Repository<User>();
-            repo.Save(this);
-        }
-
-        public void Update()
-        {
-            var repo = new Repository<User>();
-            repo.Save(this);
-        }
-
-        public void Delete()
-        {
-            var repo = new Repository<User>();
-            repo.Delete(this);
-        }
     }
 }
---
+++ Lektion32/Models/Helpers/EmailHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lektion32.Models.Helpers
+{
+    public class EmailHelper
+    {
+        public static void SendEmail(string Email, string Subject, string Message)
+        {
+            // Implementation for sending Email here
+        }
+    }
+}
