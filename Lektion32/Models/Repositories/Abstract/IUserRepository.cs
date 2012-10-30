91056f605f5a02ee22e516facfbb6a37cebf18c2
David Gullmarsvik <awt2.2012@gmail.com>
2012-10-30 12:49:35 +0100

    Uppgift 4

---
+++ Lektion32/Models/Repositories/Abstract/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lektion32.Models.Entities;
+
+namespace Lektion32.Models.Repositories.Abstract
+{
+    public interface IUserRepository
+    {
+        User FindUserByEmail(string email);
+    }
+}
---
+++ Lektion32/Models/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lektion32.Models.Entities;
+using Lektion32.Models.Repositories.Abstract;
+
+namespace Lektion32.Models.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public User FindUserByEmail(string Email)
+        {
+            return FindAll(u => u.Email == Email).FirstOrDefault();
+        }
+    }
+}
